8e77efca22479b2d9348e9bdc5fba75f700774bd
Santu <1santu1@gmail.com>
2022-09-06 00:54:52 +0530

    Third commit

--- StringClasses/src/com/Santhosh/stringClasses/DisplayIndividualLetters.java
+++ StringClasses/src/com/Santhosh/stringClasses/DisplayIndividualLetters.java
@@ -1,22 +1,24 @@
 package com.Santhosh.stringClasses;
 
+import java.util.Scanner;
+
 public class DisplayIndividualLetters {
 
 	public static void main(String[] args) {
-		// Scanner scanner = new Scanner(System.in);
-		// System.out.println("Type text as your own: ");
-		// String something = scanner.next();
-		String something = "Hello World";// trail and error here
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type text as your own: ");
+		String something = scanner.nextLine();
 		int lastValue = something.length();
-		// do {
-		// System.out.println(something.substring(indexValue, (indexValue + 1)));
-		// indexValue++;
-		// } while (indexValue < lastValue);
+		int indexValue = 0;
 		// for (int i = 0; i <= lastValue; i++) {// Pyramid Printing code
 		// System.out.println(something.substring(0, (i + 1)));
 		// }
-		for (int i = 0; i <= lastValue; i++) {
+		for (int i = 0; i < lastValue; i++) {
 			System.out.println(something.charAt(i));
 		}
+		// while (indexValue < lastValue) {// using while loop
+		// System.out.println(something.substring(indexValue, (indexValue + 1)));
+		// indexValue++;
+		// }
 	}
 }
